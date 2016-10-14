e5eea186bd66dc2f3e501610c919731159ec23d3
ieee0824 <ieee0824@ezweb.ne.jp>
2016-10-14 10:28:09 +0900

    build test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -e
 
-FANLIN_VERSION="v1.1.0"
-HASH=$(git rev-parse --verify HEAD)
-BUILDDATE=$(date '+%Y/%m/%d %H:%M:%S %Z')
-GOVERSION=$(go version)
-
 git clone https://github.com/wfarr/goenv.git $HOME/.goenv 
 export PATH=$HOME/.goenv/bin:$PATH
 eval "$(goenv init -)"
@@ -15,10 +10,16 @@ goenv global 1.7.1
 mkdir -p $HOME/go
 
 export GOPATH=$HOME/go
+FANLIN_VERSION="v1.1.0"
+HASH=$(git rev-parse --verify HEAD)
+BUILDDATE=$(date '+%Y/%m/%d %H:%M:%S %Z')
+GOVERSION=$(go version)
+
+
 
 go get -v github.com/livesense-inc/fanlin/cmd/fanlin
 
-GOOS=linux GOARCH=amd64 go build -ldflags "-X \"main.buildVersion=${FANLIN_VERSION}\" -X \"main.buildDate=${BUILDDATE}\" -X \"main.goversion=${GOVERSION}\"" github.com/livesense-inc/fanlin/cmd/fanlin
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X \"main.buildVersion=${FANLIN_VERSION}\" -X \"main.buildDate=${BUILDDATE}\" -X \"main.goversion=${GOVERSION}\"" github.com/livesense-inc/fanlin/cmd/fanlin
 
 ls -a
 
